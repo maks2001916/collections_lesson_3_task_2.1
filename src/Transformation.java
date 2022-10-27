bce9e486b1fc2595fee55bb1fc491d29f1449fe8
maks <mamonov.maksim.20003@gmail.com>
2022-10-27 18:55:08 +0500

    задание доработано 0.1

--- src/Transformation.java
+++ src/Transformation.java
@@ -4,42 +4,33 @@ public class Transformation {
     Map<String, List<Integer>> plenty;
     List<Integer> list;
     private String key;
-    private int sum;
     Random random = new Random();
     int sum1 = 0;
     List<Integer> list1 = new ArrayList<>();
-    int variableOne = 0;
-    int variableTwo = 0;
-    int variableThree = 0;
+    Map<String, List<Integer>> plentyFinis = new HashMap<>();
     private void randoms() {
-
         int one = random.nextInt(1000);
         int two = random.nextInt(1000);
         int three = random.nextInt(1000);
         list1.add(one);
         list1.add(two);
         list1.add(three);
-        variableOne = one;
-        variableTwo = two;
-        variableThree = three;
-
     }
 
     public Transformation(String plenty) {
-        Map<String, List<Integer>> plentyFinis = new HashMap<>();
         plentyFinis.put(plenty, list1);
         key = plenty;
     }
 
-    private void calculatingTheAmount() {
-        sum1 = variableOne + variableTwo + variableThree;
-    }
-
     public void lineUp() {
         randoms();
-        calculatingTheAmount();
+        int keyAddress = 0;
         Map<String, Integer> lineUp = new HashMap<>();
-        lineUp.put(key, sum1);
+        for (int i = 0; i < list1.size(); i++) {
+            sum1 = sum1 + plentyFinis.get(key).get(keyAddress);
+            lineUp.put(key, sum1);
+            keyAddress ++;
+        }
         System.out.println(lineUp);
     }
 
